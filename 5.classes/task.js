b740ba361a89a9f2712c0d3885f54627fae2ba7f
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-06-10 23:02:51 +0300

    Update task - 4.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -89,3 +89,41 @@ class Library {
         return null;
     }
 }
+
+class Student {
+    constructor(name) {
+        this.name = name;
+        this.marks = {};
+    }
+
+    addMark(mark, subject) {
+        if (mark < 2 || mark > 5) {
+            return;
+        }
+
+        if (!this.marks[subject]) {
+            this.marks[subject] = [];
+        }
+
+        this.marks[subject].push(mark);
+    }
+
+    getAverageBySubject(subject) {
+        if (!this.marks[subject] || this.marks[subject].length === 0) {
+            return 0;
+        }
+
+        const total = this.marks[subject].reduce((sum, mark) => sum + mark, 0);
+        return total / this.marks[subject].length;
+    }
+
+    getAverage() {
+        const subjects = Object.keys(this.marks);
+        if (subjects.length === 0) {
+            return 0;
+        }
+
+        const totalAverage = subjects.reduce((sum, subject) => sum + this.getAverageBySubject(subject), 0);
+        return totalAverage / subjects.length;
+    }
+}

279e922913e6ca477e0b2e03439cbfbc1a0c9197
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-06-10 23:21:14 +0300

    Update task 4-4.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,7 +11,7 @@ Student.prototype.setSubject = function(subjectName) {
 
 Student.prototype.addMarks = function(...marksToAdd) {
     if (!this.marks) {
-        return;
+        this.marks = [];
     }
     this.marks.push(...marksToAdd);
 }
@@ -26,6 +26,6 @@ Student.prototype.getAverage = function() {
 
 Student.prototype.exclude = function(reason) {
     delete this.subject;
-    delete this.marks;
+    this.marks = [];
     this.excluded = reason;
 }

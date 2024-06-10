92c74870bb861a45479f9f26b3568f6bd58b41d6
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-06-10 23:27:14 +0300

    Update task 4-7.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,7 +10,7 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marksToAdd) {
-    if (!this.marks) {
+    if (this.excluded) {
         return;
     }
     this.marks.push(...marksToAdd);

8f5f610665dc8645b1148c5160333a167a72cad9
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-06-10 23:14:35 +0300

    Update task 4-2.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,26 +5,26 @@ function Student(name, gender, age) {
     this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
+Student.prototype.setSubject = function(subjectName) {
    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function(...marksToAdd) {
    if (!this.marks) {
-        this.marks = [];
+        return;
     }
     this.marks.push(...marksToAdd);
 }
 
-Student.prototype.getAverage = function () {
+Student.prototype.getAverage = function() {
   if (!this.marks || this.marks.length === 0) {
         return 0;
     }
-    const sum = this.marks.reduce((acc, value) => acc + value, 0);
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
     return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
+Student.prototype.exclude = function(reason) {
   delete this.subject;
     delete this.marks;
     this.excluded = reason;
